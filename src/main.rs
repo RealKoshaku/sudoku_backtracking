0a224c65dc4c92989540dbcf09f3bf3307f674f7
Adahy Plas <plasadahy0@gmail.com>
2026-08-27 12:25:41 +0200

    Renamed sudokuGrid

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-mod sudokuGrid;
-use sudokuGrid::Sudoku;
+mod sudoku_grid;
+use sudoku_grid::Sudoku;
 
 fn main() {
-    let sudoku = Sudoku::new([[0; 9]; 9]);
+    let sudoku = Sudoku::new([[10; 9]; 9]);
     match sudoku.is_valid() {
         true => println!("Is valid !"),
         false => println!("Is not valid !"),
