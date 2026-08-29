f9421636dbcede280bd889bbacb90ea0f8328437
Adahy Plas <plasadahy0@gmail.com>
2026-08-30 01:28:33 +0200

    Renamed sudoku_grid and modified name of is_valid to is_number_valid

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use sudoku_grid::Sudoku;
 
 fn main() {
     let sudoku = Sudoku::new([[10; 9]; 9]);
-    match sudoku.is_valid() {
+    match sudoku.is_number_valid() {
         true => println!("Is valid !"),
         false => println!("Is not valid !"),
     }
